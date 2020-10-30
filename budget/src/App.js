89b8939c1918950390122e42124c766a5f9dda7b
Almlett <alan.andrade@tectijuana.edu.mx>
2020-10-30 12:15:19 -0700

    video 70-73

--- budget/src/App.js
+++ budget/src/App.js
@@ -1,10 +1,16 @@
+import React from 'react';
+import Question from './components/Question.jsx';
 
 function App() {
   return (
     <div className="container">
       <header>
         <h1>Budget</h1>
+        
       </header>
+        <div className="contenido-principal contenido">
+          <Question />
+        </div>
     </div>
     
   );
---
+++ budget/src/components/Question.jsx
@@ -0,0 +1,48 @@
+import React, { Fragment, useState } from 'react'
+import Error from './Error';
+
+const Question = () => {
+
+    const [quantity, setQuantity] = useState(0);
+    const [error, setError] = useState(false)
+
+    const addBudget = e => {
+        e.preventDefault();
+        //validar
+        if ( quantity < 1 || isNaN( quantity ) ){
+            setError(true);
+            return;
+        }
+        setError(false);
+    };
+
+    return ( 
+        <Fragment>
+            <h2>
+                What is your budget?
+            </h2>
+            {
+                error && <Error message="An error ocurred"/>
+            }
+
+            <form
+                onSubmit={addBudget}
+            >
+                <input
+                    type="number"
+                    className="u-full-width"
+                    placeholder="budget"
+                    onChange={e => setQuantity( parseInt( e.target.value,10 ) )}
+                />
+
+                <input
+                    type="submit"
+                    className="button-primary u-full-width"
+                    value="Set budget"
+                />
+            </form>
+        </Fragment>
+     );
+}
+ 
+export default Question;
