ec9edc782e0dca5e3e368cb08d8375ce0385ab0a
Almlett <alan.andrade@tectijuana.edu.mx>
2020-11-03 07:53:57 -0800

    Add style components

--- quote/src/App.js
+++ quote/src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import Header from './components/Header.jsx';
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`
+
+const FormContainer = styled.div`
+  background-color: #FFFFFF;
+  padding: 3rem;
+`
 
 function App() {
   return (
-    <Header 
-      title="Quote"
-    />
+    <Container>
+      <Header 
+        title="Quote"
+      />
+      <FormContainer/>
+    </Container>
   );
 }
 
--- quote/src/components/Header.jsx
+++ quote/src/components/Header.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import styled from '@emotion/styled';
+
+const HeaderContent = styled.header`
+    background-color: #26C6DA;
+    padding: 10px;
+    font-weight: bold;
+    color: #FFFFFF;
+`
+const H1Content = styled.h1`
+    font-size:2rem;
+    margin: 0;
+    font-family: 'Slabo 27px', serif;
+    text-align: center;
+`
 
 const Header = ({ title }) => {
     return ( 
-        <header>
-            <h1>{title}</h1>
-        </header>
+        <HeaderContent>
+            <H1Content>{title}</H1Content>
+        </HeaderContent>
      );
 }
  
