58f118826b256245e904020d2443677e6e6a46de
Almlett <alan.andrade@tectijuana.edu.mx>
2020-11-05 13:04:40 -0800

    finish pixabay project

--- 8-pixabay/src/App.js
+++ 8-pixabay/src/App.js
@@ -1,15 +1,91 @@
-import React from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Form from './components/Form.jsx';
+import ImageList from './components/ImageList.jsx';
 
 function App() {
+
+  const [search, setSearch] = useState('');
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [pages, setPages] = useState(1);
+
+  useEffect( () => {
+    if (search === '') return;
+
+    const ConsultAPI = async () => {
+      const imagePerPage = 30;
+      const API_KEY = '11472072-424841c62e89511587224c014';
+      const url = `https://pixabay.com/api/?key=${API_KEY}&q=${search}&per_page=${imagePerPage}&page=${page}`;
+
+      const response = await fetch(url);
+      const result = await response.json();
+      setImages(result.hits)
+      const totalPages = Math.ceil(result.totalHits / imagePerPage);
+      setPages(totalPages === 0 ? 1: totalPages)
+
+      const jumbotron = document.querySelector('.jumbotron');
+      jumbotron.scrollIntoView({ behavior:'smooth'})
+      
+    };
+    ConsultAPI();
+  }, [search,page])
+
+  const previousPage = () => {
+    const newActualPage = page - 1 > 0 ? page - 1 : 1;
+    setPage(newActualPage)
+  };
+  const nextPage = () => {
+    const newActualPage = page + 1 < pages ? page + 1 : pages;
+    setPage(newActualPage)
+  };
+
+  
+
   return (
     <div className="container">
       <div className="jumbotron">
         <p className="lead text-center">
           Image search
         </p>
-        <Form />
+        <Form 
+          setSearch={setSearch}
+        />
+
+        
       </div>
+
+      
+        
+        <div className="row justify-content-center">
+          <ImageList 
+            images={images}
+          />
+          { pages !== 1 &&
+            <Fragment>
+              { page !== 1 &&
+                <button
+                  type="button"
+                  className="btn btn-info mr-1"
+                  onClick={previousPage}
+                >
+                  &laquo; Previous 
+                </button>
+              }
+
+              { page !== pages &&
+                <button
+                  type="button"
+                  className="btn btn-info "
+                  onClick={nextPage}
+                >
+                  Next &raquo;
+                </button>
+              }
+            </Fragment>
+          }
+
+        </div>
+
     </div>
   );
 }
--- 8-pixabay/src/components/Form.jsx
+++ 8-pixabay/src/components/Form.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Error from './Error.jsx';
 
-const Form = () => {
+const Form = ({setSearch}) => {
+
+    const [term , setTerm] = useState('');
+    const [error , setError] = useState(false);
+    
+
+    const searchImages = e => {
+        e.preventDefault();
+
+        if (term.trim() === ''){
+            setError(true);
+            return;
+        }
+        setError(false);
+        setSearch(term)
+    }
+
     return ( 
-        <form>
+        <form
+            onSubmit={searchImages}
+        >
             <div className="row">
                 <div className="form-group col-md-8">
                     <input 
                         type="text"
                         className="form-control form-control-lg"
                         placeholder="Search a image, example: dev"
+                        onChange={ e => setTerm(e.target.value)}
                     />
                 </div>
                 <div className="form-group col-md-4">
@@ -19,6 +39,13 @@ const Form = () => {
                     />
                 </div>
             </div>
+
+            {
+                error &&
+                <Error 
+                    message='add text to search'
+                />
+            }
         </form>
      );
 }
