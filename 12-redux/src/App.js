a03e4d7f42e781d9fd883f64d5d08677593814b7
Almlett <alan.andrade@tectijuana.edu.mx>
2020-12-22 08:34:19 -0800

    redux v1

--- 12-redux/src/App.js
+++ 12-redux/src/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Products from './components/Products.jsx';
+import Header from './components/Header.jsx';
+import NewProduct from './components/NewProduct.jsx';
+import EditProduct from './components/EditProduct.jsx';
+
 
 function App() {
   return (
-    <h1>Redux</h1>
+    <Router>
+      <Header />
+      <div className="container">
+        <Switch>
+          <Route exact path ="/" component={Products} />
+          <Route exact path ="/products/new" component={NewProduct} />
+          <Route exact path ="/products/edit/:id" component={EditProduct} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
